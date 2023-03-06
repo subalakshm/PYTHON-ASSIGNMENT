c376685090e16cc95dd53193ab9d9a9b8932089e
subalakshm <127026775+subalakshm@users.noreply.github.com>
2023-03-07 00:17:07 +0530

    Add files via upload

---
+++ even and odd.py
@@ -0,0 +1,17 @@
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+
+even, odd= 0, 0
+
+for i in l: 
+
+    if i % 2 == 0: 
+
+        even += 1
+
+    else: 
+
+        odd+= 1          
+
+print("Even : ", even) 
+
+print("Odd : ", odd)
